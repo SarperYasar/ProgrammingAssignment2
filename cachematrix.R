8360fde37a1e5162a03dda9d0986450814d0c187
sarperyasar@gmail.com <sarperyasar@gmail.com>
2020-10-26 19:07:09 +0300

    Matrix Inverser that simply caches or returns the cached one if exists

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## makeCacheMatrix <- function(x = matrix()) {}
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve <- function(x, ...) {## Return a matrix that is the inverse of 'x'}
+
+makeCacheMatrix <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() {x}
+  setInv <- function(inverse) {inv <<- inverse}
+  getInv <- function() {inv}
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
+}
+
+cacheSolve <- function(x, ...){
+  inv <- x$getInv()
+  if(!is.null(inv)){
+    message("Below is from the cache")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInv(inv)
+  inv
 }
